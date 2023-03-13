ea5f964d6bc60d603ad6ccd824d605470d9d5971
ShikhaPrakashJha <shikha.prakash1994@gmail.com>
2023-03-13 13:06:46 +0530

    commit done

---
+++ src/test/java/githubJenkinsPractice/Test01.java
@@ -0,0 +1,18 @@
+package githubJenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class Test01 {
+	@Test
+	public void firstTest() {
+		System.out.println("This is firstTest");
+	}
+	@Test
+	public void secondTest() {
+		System.out.println("This is secondTest");
+	}
+	@Test
+	public void thirdTest() {
+		System.out.println("This is thirdTest");
+	}
+}
---
+++ src/test/java/githubJenkinsPractice/test02.java
@@ -0,0 +1,19 @@
+package githubJenkinsPractice;
+
+import org.testng.annotations.Test;
+
+public class test02 {
+
+	@Test
+	public void fourthTest() {
+		System.out.println("This is fourthTest");
+	}
+	@Test
+	public void fifthTest() {
+		System.out.println("This is fifthTest");
+	}
+	@Test
+	public void sixthTest() {
+		System.out.println("This is sixthTest");
+	}
+}
